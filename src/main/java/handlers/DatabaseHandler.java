db58e5952300cca3d782b8cf7f5cac86ba60f9da
ArkSa <arkadiusz.salega@gmail.com>
2021-11-22 10:44:36 +0100

    Deleted MenuHandler class(not needed)

--- src/main/java/handlers/DatabaseHandler.java
+++ src/main/java/handlers/DatabaseHandler.java
@@ -5,7 +5,7 @@ import models.EmployeeDatabase;
 public class DatabaseHandler {
     public static void fillEmployeeDatabase(EmployeeDatabase employeeDatabase) {
         System.out.println("Running PayRoll application." + "\nPlease add 5 new employees to pay roll.");
-        for (int i = 0; i < 1; i++) {
+        for (int i = 0; i < 5; i++) {
             System.out.println("Employee number " + (i + 1));
             employeeDatabase.addNewEmployee(UserInputHandler.getEmployeeFromUser(employeeDatabase));
         }
